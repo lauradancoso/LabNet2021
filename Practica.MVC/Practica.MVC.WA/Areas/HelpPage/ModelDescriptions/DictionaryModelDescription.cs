68b15ad720724f12efe8d0e2d08d10e434202625
lauradancoso <lauradancoso.neorislab.2021@gmail.com>
2021-05-13 17:09:27 -0300

    clean: borré files que no pertenecian a la rama y estaban por error

--- Practica.MVC/Practica.MVC.WA/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Practica.MVC.WA.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Practica.MVC/Practica.MVC.WA/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica.MVC.WA.WebApiApplication" Language="C#" %>
