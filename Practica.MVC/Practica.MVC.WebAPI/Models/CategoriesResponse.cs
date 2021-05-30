df09d709605b87c1b27db93f4de227982c861d5d
lauradancoso <lauradancoso.neorislab.2021@gmail.com>
2021-05-30 19:15:01 -0300

    fix: change CategoriesRequest to CategoriesResponse

---
+++ Practica.MVC/Practica.MVC.WebAPI/Models/CategoriesResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practica.MVC.WebAPI.Models
+{
+    public class CategoriesResponse
+    {
+        public int CategoryID { get; set; }
+
+        [Required]
+        [StringLength(15), MinLength(1)]
+        [Display(Name = "Name")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+    }
+}
